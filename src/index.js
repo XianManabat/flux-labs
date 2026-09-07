f05fe733cfd62d4fcae337778aaf098abdd04ee6
Xian Manabat <xianmananat@example.com>
2026-09-07 21:27:04 +0800

    try

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '..' , 'public')));
 app.listen(PORT, () =>{
     console.log(`Server running on port ${PORT}`);
