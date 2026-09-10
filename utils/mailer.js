06dd0dbf87a715bb60c61ccfdbf6834ab3f0031f
Xian Manabat <xianmananat@example.com>
2026-09-10 08:10:41 +0800

    WAG GAGALAWIN: email sending

--- utils/mailer.js
+++ utils/mailer.js
@@ -6,6 +6,7 @@ const transporter = nodeMailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     }
+    
 });
 
 function verificationCode( toEmail, code ) {
